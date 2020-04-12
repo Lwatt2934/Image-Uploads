81ebece92b3553f4bfe27c8eaefbca1c18f95fc7
L TW <lwat413@gmail.com>
2020-04-12 13:02:28 -0500

    Code done.

--- ImageUploads/Form1.cs
+++ ImageUploads/Form1.cs
@@ -16,5 +16,31 @@ namespace ImageUploads
         {
             InitializeComponent();
         }
+
+        private void image_UploadsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.image_UploadsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.myImages);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'myImages.Image_Uploads' table. You can move, or remove it, as needed.
+            this.image_UploadsTableAdapter.Fill(this.myImages.Image_Uploads);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFd = new OpenFileDialog();
+            openFd.Filter = "Images Only. | *.jpg; *.jpeg; *.png; *.gif;";
+
+            DialogResult dr = openFd.ShowDialog();
+
+            imgPictureBox.Image = Image.FromFile(openFd.FileName);
+            imgpathLabel1.Text = openFd.FileName;
+        }
     }
 }
